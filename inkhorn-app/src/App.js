f20cc263dbfc45723d6a5bc9aecde03eefcb8af5
Katherine Graham <katherinegraham@Katherines-MacBook.local>
2019-09-05 15:30:09 -0700

    Adding more touches

--- inkhorn-app/src/App.js
+++ inkhorn-app/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Navbar, Nav, Carousel, Container, Row, Col, Image } from 'react-bootstrap';
+import Home from './pages/home/Home.js';
+import About from './pages/about/About.js';
+import Careers from './pages/careers/Careers.js';
+import Contact from './pages/contact/Contact.js';
+import Practices from './pages/practices/Practices.js';
 import './App.scss';
 
 function App() {
@@ -15,16 +21,17 @@ function App() {
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
             crossorigin="anonymous"
         />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
         <header className="sticky-header">    
             <Navbar className="navbar-color" expand="lg">
-                <Navbar.Brand href="/">Inkhorn Legal Group</Navbar.Brand>
+                <Navbar.Brand href="/">Inkhorn</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className="inkhorn-navbar" id="basic-navbar-nav">
                     <Nav className="justify-content-end">
-                        <Nav.Link href="home">Home</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="practices">Practices</Nav.Link>
-                        <Nav.Link href="people">People</Nav.Link>
+                        {/*Coming Soon: <Nav.Link href="people">People</Nav.Link>*/}
                         <Nav.Link href="careers">Careers</Nav.Link>
                         <Nav.Link href="about">About</Nav.Link>
                         <Nav.Link href="contact">Contact</Nav.Link>
@@ -37,54 +44,27 @@ function App() {
             </div>
         </header>
 
-        <div className="carousel-container">
-            <Carousel>
-                <Carousel.Item>
-                    <img className="d-block w-100" src="/San-Diego-Images/Hall-Of-Justice-5.jpg" alt="First slide" />
-                    <Carousel.Caption>
-                        <h3>Integrity</h3>
-                        <p>Law Office of Jeremiah Graham</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img className="d-block w-100" src="/San-Diego-Images/New-Courthouse-3.jpg" alt="Third slide" />
-                    <Carousel.Caption>
-                        <h3>Results</h3>
-                        <p>Law Office of Jeremiah Graham</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img className="d-block w-100" src="/San-Diego-Images/Skyline-3.jpg" alt="Third slide" />
-                    <Carousel.Caption>
-                        <h3>Fairness</h3>
-                        <p>Law Office of Jeremiah Graham</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </div>
+        <Router>
+            <div className="content">
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/careers" component={Careers} />
+                <Route exact path="/contact" component={Contact} />
+                <Route exact path="/practices" component={Practices} />
+            </div>
+        </Router>
+        
+        <footer className="inkhorn-footer">
+            <div className="copyright">&copy; <b>Inkhorn Legal Group</b> All Rights Reserved </div>
+            <div className="social-media">
+                <a href="#" class="fa fa-youtube"></a>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-instagram"></a>
+                <a href="#" class="fa fa-linkedin"></a>
+            </div>
+        </footer>
 
-        <div className="card-container">
-            <Container>
-                <Row>
-                    <Col xs={5} md={3}>
-                        <Image src="/San-Diego-Images/Coronado-Bridge-2.jpg" thumbnail />
-                        <h3>Contact</h3>
-                    </Col>
-                    <Col xs={5} md={3}>
-                        <Image src="/San-Diego-Images/New-Courthouse-7.jpg" thumbnail />
-                        <h3>Freedom of Conscience</h3>
-                    </Col>
-                    <Col xs={5} md={3}>
-                        <Image src="/San-Diego-Images/Law-Corner.jpg" thumbnail />
-                        <h3>Promotional Offers</h3>
-                    </Col>
-                    <Col xs={5} md={3}>
-                        <Image src="/San-Diego-Images/San-Diego-Glamour-1.jpg" thumbnail />
-                        <h3>Vacation Default Judgements</h3>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
     </div>
   );
 }
