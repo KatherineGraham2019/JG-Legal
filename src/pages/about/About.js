05b6a0387d9dfaed37d8b3e04533293ac04fe4fe
Katherine Graham <katherinegraham@Katherines-MacBook.local>
2019-09-05 16:10:14 -0700

    Ad of slugignore, fix about

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -19,12 +19,14 @@ class About extends React.Component {
                     </div>
                     
                     <article className="overview-info">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        This is the law firm you have been looking for, with first rate service and affordable rates, 
+                        Inkhorn is the future of litigation in San Diego. As an upstart San Diego business law firm, 
+                        we specialize as litigators and trial lawyers and are working towards offering full-service legal 
+                        advice to our clients. 
+                        <br />
+                        Every member of our staff signs a pledge to uphold the highest standard of ethics and morality, 
+                        and the principles or our practice are time-honored traditions of honesty, integrity, transparency, 
+                        diligence and focus. We believe that by serving our clients, we are serving God.
                     </article>
                 </div>
 
